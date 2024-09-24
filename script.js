da1a264e208549533ed701b9b6651ad2a20da988
Zubizeru <jose.ehmendes@senacsp.edu.br>
2024-09-24 18:52:41 -0300

    feat: atualizando com comentários

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ Regras:
 Todos os campos devem ser válidos e cumprir os requisitos mínimos de validação. */
 inicio();
 
+// Função de iniciar algoritmo, ela ira iniciar as funções principais, como  cadastrarCliente e exibirCadastroCliente.
 function inicio() {
     let nomes = [];
     let cpfs = [];
@@ -24,6 +25,7 @@ function inicio() {
     exibirCadastroCliente(nomes, cpfs, enderecos);
 }
 
+// Função de cadastro de cliente, ela ira iniciar as funções auxiliares, como cadastrarNomes, cadastrarCpfs e cadastrarEnderecos.
 function cadastrarCliente(nomes, cpfs, enderecos) {
     let nome = cadastrarNomes();
     let cpf = cadastrarCpfs();
@@ -34,6 +36,7 @@ function cadastrarCliente(nomes, cpfs, enderecos) {
     enderecos.push(endereco);
 }
 
+// Função de cadastro de nome, não pode deixar o prompt vazio, e o espaço(" ") tem que estar no 3°length ou mais para ser válido.
 function cadastrarNomes() {
     let nome;
     do {
@@ -45,27 +48,31 @@ function cadastrarNomes() {
     return nome;
 }
 
+// Função de cadastro de cpfs, não pode deixar o prompt vazio, tem que ter 11°length, precisa ter números e precisa estar no formato "xxx.xxx.xxx-xx"..
 function cadastrarCpfs() {
     let cpf;
     do {
         cpf = prompt("Informe o CPF (apenas números):").trim();
-        if (cpf.length !== 11 || isNaN(cpf)) {
+        if (cpf === "" || cpf.length !== 11 || isNaN(cpf)) {
             alert("CPF inválido. Informe o CPF novamente (11 dígitos numéricos).");
         }
-    } while (cpf.length !== 11 || isNaN(cpf));
+    } while (cpf === "" || cpf.length !== 11 || isNaN(cpf));
     return `${cpf.slice(0, 3)}.${cpf.slice(3, 6)}.${cpf.slice(6, 9)}-${cpf.slice(9, 11)}`;
 }
 
+// Função de cadastro de endereços.
 function cadastrarEnderecos() {
+    // Cadastro de cep, não pode deixar o prompt vazio, tem que ter 8°length, precisa ter números e precisa estar no formato "xxxxx-xxx".
     let cep;
     do {
         cep = prompt("Informe o CEP:").trim();
-        if (cep.length !== 8 || isNaN(cep)) {
+        if (cep === "" || cep.length !== 8 || isNaN(cep)) {
             alert("CEP inválido. Informe o CEP corretamente (8 dígitos numéricos).");
         }
-    } while (cep.length !== 8 || isNaN(cep));
+    } while (cep === "" || cep.length !== 8 || isNaN(cep));
     cep = `${cep.slice(0, 5)}-${cep.slice(5, 8)}`
 
+    // Cadastro de logradouro, não pode deixar o prompt vazio. 
     let logradouro;
     do {
         logradouro = prompt("Informe o Logradouro:").trim();
@@ -74,6 +81,7 @@ function cadastrarEnderecos() {
         }
     } while (logradouro === "");
 
+    // Cadastro de número, não pode deixar o prompt vazio e precisa ter números.
     let numero;
     do {
         numero = prompt("Informe o Número:").trim();
@@ -82,8 +90,10 @@ function cadastrarEnderecos() {
         }
     } while (numero === "" || isNaN(numero));
 
+    // Cadastro de complemento, opcional.
     let complemento = prompt("Informe o Complemento (opcional):").trim();
 
+    // Cadastro de bairro, não pode deixar o prompt vazio.
     let bairro;
     do {
         bairro = prompt("Informe o Bairro:").trim();
@@ -92,6 +102,7 @@ function cadastrarEnderecos() {
         }
     } while (bairro === "");
 
+    // Cadastro de cidade, não pode deixar o prompt vazio.
     let cidade;
     do {
         cidade = prompt("Informe a Cidade:").trim();
@@ -100,6 +111,7 @@ function cadastrarEnderecos() {
         }
     } while (cidade === "");
 
+    // Cadastro de uf, ter que tem uma length igual a 2 e precisa ser válido.
     let uf;
     const enderecouf = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
     do {
@@ -112,6 +124,7 @@ function cadastrarEnderecos() {
     return { cep, logradouro, numero, complemento, bairro, cidade, uf };
 }
 
+// Função de exibir cadastro do cliente.
 function exibirCadastroCliente(nomes, cpfs, enderecos) {
     document.write("<h2>Cadastro de Clientes</h2>");
     for (let i = 0; i < nomes.length; i++) {
